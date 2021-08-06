8063732bfa052d54c974f3632888c7ab71e49c08
Erick <erick_vinicius_04@hotmail.com>
2021-08-06 09:33:58 -0300

    Pedra, papel e tesoura contra um BOT

---
+++ Jokenpo.py
@@ -0,0 +1,31 @@
+# fazer um jogo programa que simula um jogo de "pedra, papel e tesoura"
+# entre você e a máquina
+# retorna quem é o vencedor da rodada
+import random
+lista = ('pedra','papel','tesoura')
+npc = lista[random.randint(0,2)]
+player = input('pedra papel ou tesoura?: ')
+print('A jogada do NPC foi: {}'.format(npc))
+if player == npc:
+    print('sua jogada:', player)
+    print('jogada npc:', npc)  
+    print('Empate') 
+elif player == 'pedra':
+    if npc == 'papel':
+        print('Você perdeu')
+    else:
+        print('Você ganhou')
+elif player == 'papel':
+    if npc == 'pedra':
+        print('Você ganhou')
+    else:
+        print('Você perdeu')
+elif player == 'tesoura':
+    if npc == 'papel':
+        print('Você ganhou')
+    else:
+        print('Você perdeu')
+else:
+    print('Há um erro de digitação em sua jogada')
+ 
+
